60fd8a49d08083094923d1d70ae0de29eb33c4fa
Cade Holmes <holmesc16@gmail.com>
2021-01-24 21:40:32 -0600

    development branch, local cache of data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { GlobalStyle } from './ui/AppStyles'
 import Home from './views/Home';
 import { useFetch } from './utils'
+const localdata = require('./dummy.json')
 
 function App() {
-  let {response, loading} = useFetch('http://localhost:5000', {})  
-  
+  // @ NOTE: commenting out destructure of data and loading for testing / sharing
+  // let {response, loading} = useFetch('http://localhost:5000', {})  
+  const response = localdata
+  const loading = response.length ? false : true
+  console.log(response)
   return (
       <div className="App">
               <GlobalStyle/>
